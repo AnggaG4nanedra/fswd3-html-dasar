2f8f2640c269b8db618c6408fd1db543deeb1375
AnggaG4nanedra <20042010077@student.upnjatim.ac.id>
2023-03-25 10:06:22 +0700

    Tugas 10 FSWD 1

---
+++ js/To Do List 2.js
@@ -0,0 +1,72 @@
+const baseUrl = "https://crudcrud.com/api/";
+const apiKey = "7e36265034954cbca3d638731c7390d9"
+const url = baseUrl + apiKey + "todos"
+
+let todos = [];
+
+loadTodos();
+
+function newElement () {
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+function loadTodos() {
+  fetch(url).then((response)={
+    todos=responsejson()
+    todos.forEach((todo) = {
+      createlist(todo);
+    })
+  });
+}
